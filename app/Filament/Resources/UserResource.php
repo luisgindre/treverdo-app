9a22af4481b2a5a55a1b52042464f444476ab20f
vagrant <vagrant@homestead>
2024-01-16 20:31:04 +0000

    Monto hace cagadas

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -10,9 +10,11 @@ use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Filament\Forms\Components\Select;
+#use Filament\Resources\Forms\Components\Select;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
-
+use Illuminate\Support\Facades\Hash;
 
 class UserResource extends Resource
 {
@@ -21,24 +23,50 @@ class UserResource extends Resource
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
     protected static ?string $navigationGroup = 'Administrar';
-    
-    protected static ?string $navigationLabel = 'Usuarios';
-
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
+                Forms\Components\TextInput::make('current_team_id')
+                    ->numeric(),
+                Forms\Components\TextInput::make('user_creator_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('company_id')
+                    ->numeric(),
+                #Forms\Components\Select::make('company_id')
+                #    ->relationship(name: 'company', titleAttribute: 'name'),
+                #Forms\Components\select('company_id', 'Seleccionar Empresa')
+                #->options(fn () => \App\Models\Company::pluck('name', 'id'))
+                #->displayUsingLabels(),
                 Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('last_name')
                     ->required()
                     ->maxLength(255),
                 Forms\Components\TextInput::make('email')
                     ->email()
                     ->required()
                     ->maxLength(255),
+                Forms\Components\TextInput::make('company_position')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('work_phone')
+                    ->tel()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('work_mail')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('cell_phone')
+                    ->tel()
+                    ->maxLength(255),
                 Forms\Components\DateTimePicker::make('email_verified_at'),
+                Forms\Components\DateTimePicker::make('last_update_user_id'),
+                Forms\Components\Toggle::make('enabled')
+                    ->required(),
                 Forms\Components\TextInput::make('password')
                     ->password()
+                    ->dehydrateStateUsing(fn (string $state): string => Hash::make($state))
                     ->required()
                     ->maxLength(255),
                 Forms\Components\Textarea::make('two_factor_secret')
@@ -48,8 +76,6 @@ class UserResource extends Resource
                     ->maxLength(65535)
                     ->columnSpanFull(),
                 Forms\Components\DateTimePicker::make('two_factor_confirmed_at'),
-                Forms\Components\TextInput::make('current_team_id')
-                    ->numeric(),
                 Forms\Components\TextInput::make('profile_photo_path')
                     ->maxLength(2048),
             ]);
@@ -59,11 +85,41 @@ class UserResource extends Resource
     {
         return $table
             ->columns([
+                Tables\Columns\TextColumn::make('current_team_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('user_creator_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('company_id')
+                    ->numeric()
+                    ->sortable(),
                 Tables\Columns\TextColumn::make('name')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('last_name')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('company_position')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('work_phone')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('work_mail')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('cell_phone')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email_verified_at')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('last_update_user_id')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\IconColumn::make('enabled')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('two_factor_confirmed_at')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('profile_photo_path')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
---
+++ database/migrations/2024_01_16_000000_create_module_role_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('module_role_user', function (Blueprint $table) {
+            $table->foreignId('user_id')->nullable();
+            $table->foreignId('role_id')->nullable();
+            $table->foreignId('module_id')->nullable();
+            $table->timestamp('user_role_module_since');
+            $table->timestamp('user_role_module_till')->nullable();
+            $table->foreignId('user_grantor_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     * 
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('module_role_user');
+    }
+};
